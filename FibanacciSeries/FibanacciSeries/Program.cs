dfa6eb23345ddf3df62cd9ba10c5fa6259976a53
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-08-01 00:01:52 +0530

    implemented CouponNumber

--- FibanacciSeries/FibanacciSeries/Program.cs
+++ FibanacciSeries/FibanacciSeries/Program.cs
@@ -1,23 +1,49 @@
 ﻿using System;
-
-namespace ReverseNumber
+using System.Collections.Generic;
+    using System.Collections;
+using System.Linq;
+namespace CouponNumber
 {
     class Program
     {
+        static Random random = new Random();
+
         static void Main(string[] args)
         {
-            
-            int n, reverse = 0, rem;
-            Console.Write("Enter a number: ");
-            n = int.Parse(Console.ReadLine());
-            while (n != 0)
-            {
-                rem = n % 10;
-                reverse = reverse * 10 + rem;
-                n /= 10;
-            }
-            Console.Write("Reversed Number: " + reverse);
 
+
+            int coupon = 10;
+            int NumberOfcoupon = 10;
+
+
+            List<string> generatedcoupon = new List<string>();
+            char[] keys = "ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890".ToCharArray();
+
+
+            Console.WriteLine("Coupons: ");
+            while (generatedcoupon.Count < coupon)
+            {
+                var voucher = GenerateVoucher(keys, NumberOfcoupon);
+                if (!generatedcoupon.Contains(voucher))
+                {
+                    generatedcoupon.Add(voucher);
+                    Console.WriteLine("\t[#{0}] {1}", generatedcoupon.Count, voucher);
+                }
+            }
+
+            Console.WriteLine("done");
+
+            Console.ReadLine();
         }
+
+        private static string GenerateVoucher(char[] keys, int lengthOfVoucher)
+        {
+            return Enumerable
+                .Range(1, lengthOfVoucher) // for(i.. ) 
+                .Select(k => keys[random.Next(0, keys.Length - 1)])  // generate a new random char 
+                .Aggregate("", (e, c) => e + c); // join into a string
+        }
+
     }
 }
+
