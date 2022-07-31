83b044c03dd5f4c7f8c7b7dc0f835c447a9a51ec
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-07-31 23:15:22 +0530

    implemented PerfectNumber

--- FibanacciSeries/FibanacciSeries/Program.cs
+++ FibanacciSeries/FibanacciSeries/Program.cs
@@ -6,17 +6,22 @@ namespace FibanacciSeries
     {
         static void Main(string[] args)
         {
-            int n1 = 0, n2 = 1, n3;
-            Console.Write("Enter the number of element: ");
-            int number = int.Parse(Console.ReadLine());
-            Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
-            for (int i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+            Console.Write("Enter the  number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i < n; i++)
             {
-                n3 = n1 + n2; //0+1=1
-                Console.Write(n3 + " ");
-                n1 = n2; //n1=1
-                n2 = n3; //n2=1
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                }
             }
+            Console.WriteLine("The sum of the divisor is :" + sum);
+            if (sum == n)
+                Console.WriteLine("So " + n + " number is perfect.");
+            else
+                Console.WriteLine("So " + n + " number is not perfect.");
+
         }
     }
 }
