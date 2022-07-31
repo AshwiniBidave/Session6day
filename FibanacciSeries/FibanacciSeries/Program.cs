dadcc0c7bd4f02e1b562dc1e839bc7155d725f0c
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-08-01 00:24:22 +0530

    implemented StopWatchProgram

--- FibanacciSeries/FibanacciSeries/Program.cs
+++ FibanacciSeries/FibanacciSeries/Program.cs
@@ -1,47 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-    using System.Collections;
-using System.Linq;
-namespace CouponNumber
+using System.Diagnostics;
+using System.Threading;
+namespace StopWatchProgram
+
 {
     class Program
     {
-        static Random random = new Random();
-
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
 
-
-            int coupon = 10;
-            int NumberOfcoupon = 10;
+            Stopwatch stopwatch = new Stopwatch();
 
 
-            List<string> generatedcoupon = new List<string>();
-            char[] keys = "ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890".ToCharArray();
+            stopwatch.Start();
 
 
-            Console.WriteLine("Coupons: ");
-            while (generatedcoupon.Count < coupon)
+            for (int i = 0; i < 1000; i++)
             {
-                var voucher = GenerateVoucher(keys, NumberOfcoupon);
-                if (!generatedcoupon.Contains(voucher))
-                {
-                    generatedcoupon.Add(voucher);
-                    Console.WriteLine("\t[#{0}] {1}", generatedcoupon.Count, voucher);
-                }
+                Thread.Sleep(1);
             }
 
-            Console.WriteLine("done");
 
-            Console.ReadLine();
-        }
+            stopwatch.Stop();
 
-        private static string GenerateVoucher(char[] keys, int lengthOfVoucher)
-        {
-            return Enumerable
-                .Range(1, lengthOfVoucher) // for(i.. ) 
-                .Select(k => keys[random.Next(0, keys.Length - 1)])  // generate a new random char 
-                .Aggregate("", (e, c) => e + c); // join into a string
+
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+
         }
 
     }
